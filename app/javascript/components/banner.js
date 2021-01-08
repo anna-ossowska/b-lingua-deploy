081b53396a3e222d28128a52246882646cbddace
Anna Ossowska <ossowska.anna1@gmail.com>
2021-01-08 20:04:19 +0100

    Home page

--- app/javascript/components/banner.js
+++ app/javascript/components/banner.js
@@ -5,16 +5,13 @@ const loadDynamicBannerText = () => {
     return
   }
   new Typed('#banner-typed-text', {
-    strings: ["Meet a buddy, learn a language.",
+    strings: ["                    ",
+    "Meet a buddy, learn a language.",
     "Møt en venn lær et språk.",
     "Träffa en vän, lära sig ett språk.",
-    "Incontrare un amico, imparare una lingua.",
     "Møde en ven, lære et sprog.",
-    "Poznaj nowych ludzi, naucz się języka.",
-    "Einen Freund treffen, eine Sprache lernen.",     
-    "Conocer a un amigo, aprender un idioma.",
     "قابل صديق ، تعلم لغة."],
-    typeSpeed: 40,
+    typeSpeed: 30,
     loop: true, 
     showCursor: false,
   });
